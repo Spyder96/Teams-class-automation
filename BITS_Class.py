78fdb0fc7fccc665fb9693efc6d982a8c8c23009
Spyder96 <Amirul.1216@gmail.com>
2022-06-26 16:40:11 +0530

    replaced time to integer

--- BITS_Class.py
+++ BITS_Class.py
@@ -6,53 +6,88 @@ from pynput.keyboard import Controller
 from pynput.keyboard import Key
 import webbrowser as wb
 
-
+# def start_class(c):
+#     wb.open(c[0])
+#     global is_class_started=True
+#     time.sleep(20)
+#     for i in range (7):
+#         pyautogui.press('tab')
+#     time.sleep(1)
+#     pyautogui.press('enter')
+#     time.sleep(5)
     
 
 
 lst=[
-     ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_NmJhN2E2YzAtZWEwNy00OGIyLWI4YTYtNjdhODc4NTlhNWE5%40thread.v2/0?context=%7b%22Tid%22%3a%22e24ac094-efd8-4a6b-98d5-a129b32a8c9a%22%2c%22Oid%22%3a%2290c83da1-fa35-474f-ae47-5367415cc22e%22%7d', '08:55', '10:35'],
-     ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZTE1NjdjODctN2NlNy00YTM2LTllZWQtZmY4ZTcwODc4NjQ5%40thread.v2/0?context=%7b%22Tid%22%3a%22e24ac094-efd8-4a6b-98d5-a129b32a8c9a%22%2c%22Oid%22%3a%22bcc87f62-9dc8-4a1f-84be-4bcae2d8b485%22%7d', '10:55', '12:35'],
-     ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTk4MGQyNzYtN2M0Yi00ZTZjLWI0NzYtNTQwOWVlZGJmNDAw%40thread.v2/0?context=%7b%22Tid%22%3a%22e24ac094-efd8-4a6b-98d5-a129b32a8c9a%22%2c%22Oid%22%3a%226e87e067-c2c2-4036-8a2f-2c2f0973f5c7%22%7d', '13:55', '15:35'],
-     ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_YjE2NzFjMmEtNmQ1Mi00MzBhLWJkODctZmExMjI2ZGM3Mjll%40thread.v2/0?context=%7b%22Tid%22%3a%22e24ac094-efd8-4a6b-98d5-a129b32a8c9a%22%2c%22Oid%22%3a%22de1d3b87-021b-4dbb-95df-93eab2f143bd%22%7d', '15:55', '17:35']
-     
+     ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_NmJhN2E2YzAtZWEwNy00OGIyLWI4YTYtNjdhODc4NTlhNWE5%40thread.v2/0?context=%7b%22Tid%22%3a%22e24ac094-efd8-4a6b-98d5-a129b32a8c9a%22%2c%22Oid%22%3a%2290c83da1-fa35-474f-ae47-5367415cc22e%22%7d', 855, 1035],
+     ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZTE1NjdjODctN2NlNy00YTM2LTllZWQtZmY4ZTcwODc4NjQ5%40thread.v2/0?context=%7b%22Tid%22%3a%22e24ac094-efd8-4a6b-98d5-a129b32a8c9a%22%2c%22Oid%22%3a%22bcc87f62-9dc8-4a1f-84be-4bcae2d8b485%22%7d', 1055, 1235],
+     ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTk4MGQyNzYtN2M0Yi00ZTZjLWI0NzYtNTQwOWVlZGJmNDAw%40thread.v2/0?context=%7b%22Tid%22%3a%22e24ac094-efd8-4a6b-98d5-a129b32a8c9a%22%2c%22Oid%22%3a%226e87e067-c2c2-4036-8a2f-2c2f0973f5c7%22%7d', 1355, 1535],
+     ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_YjE2NzFjMmEtNmQ1Mi00MzBhLWJkODctZmExMjI2ZGM3Mjll%40thread.v2/0?context=%7b%22Tid%22%3a%22e24ac094-efd8-4a6b-98d5-a129b32a8c9a%22%2c%22Oid%22%3a%22de1d3b87-021b-4dbb-95df-93eab2f143bd%22%7d', 1555, 1735]
      
      ]
 
-#['https://teams.microsoft.com/l/meetup-join/19%3ameeting_YjEwOWNmZDMtMmQ5MC00NDM5LWFhZWUtMTY5NjZmZjJhMGI2%40thread.v2/0?context=%7b%22Tid%22%3a%226d28e4fb-9074-4a0b-a5b8-9a89f632cc60%22%2c%22Oid%22%3a%22377bd695-6601-4d9d-9bb1-bbd5cafd3784%22%7d','01:21','01:22']
+
 #input lecture stats in form of list ......
-# ["Link","start_time","end_time"]
+# ["Link", start_time, end_time ]
 # give time in 24 hrs format...
 keyboard= Controller()
+i=0
+time_left=0
 
 is_class_started =False
+
 for lecture  in lst:
-    while True :
-        if is_class_started==False:
-            if (datetime.now().hour >= int(lecture[2].split(':')[0])and 
-                datetime.now().minute > int(lecture[2].split(':')[1])):
-                break;
-            else:
-                wb.open(lecture[0])
-                is_class_started=True
-                time.sleep(20)
-                for i in range (7):
-                    pyautogui.press('tab')
-                time.sleep(1)
-                pyautogui.press('enter')
-                time.sleep(5)
-               # pyautogui.hotkey('ctrl','shift','m')
-                time.sleep(10)
-        elif   (datetime.now().hour == int(lecture[2].split(':')[0]) and
-                datetime.now().minute == int(lecture[2].split(':')[1])):
-                is_class_started=False
-                pyautogui.hotkey('ctrl','shift','h')
-                time.sleep(3)
-                pyautogui.hotkey('alt','f4')
-                time.sleep(3)
-                pyautogui.hotkey('alt','f4')
+    
+    # Checking for end timing of the selected class and skipping if required
+    #Program run after the end of selected class
+    if ((datetime.now().hour*100) + datetime.now().minute) >= lecture[2]:
+        i+=1;
+        continue
+    # while True :
+    #     if is_class_started==False:
+    #         if ((datetime.now().hour*100) + datetime.now().minute) >= lecture[2]:
+    #             break;
+    
+    #Checking for end timing of the selected class and joining if time left
+    #Program run during the selected class
+    elif (datetime.now().hour*100 + datetime.now().minute >= lecture[1]) and (((datetime.now().hour*100) + datetime.now().minute) < lecture[2]):
+        
+         wb.open(lecture[0])
+         is_class_started=True
+         time.sleep(20)
+         for i in range (7):
+             pyautogui.press('tab')
+         time.sleep(1)
+         pyautogui.press('enter')
+         time.sleep(5)
+         time_left=lecture[2]-lecture[1]
+        # pyautogui.hotkey('ctrl','shift','m')
+        
+         time.sleep(time_left*60)       #class joined, sleeping for the class period
+         
+         #exiting
+         is_class_started=False
+         pyautogui.hotkey('ctrl','shift','h')
+         time.sleep(3)
+         pyautogui.hotkey('alt','f4')
+         time.sleep(3)
+         pyautogui.hotkey('alt','f4')
+         
+         
+    #Program run before the selected                            class timings     
+    elif   (datetime.now().hour*100 + datetime.now().minute < lecture[1]):
+        
+        time_left=lecture[1]-(datetime.now().hour*100 + datetime.now().minute)
+        time.sleep(time_left*60) 
+                # is_class_started=False
+                # pyautogui.hotkey('ctrl','shift','h')
+                # time.sleep(3)
+                # pyautogui.hotkey('alt','f4')
+                # time.sleep(3)
+                # pyautogui.hotkey('alt','f4')
                 #time.sleep(3)
                 #pyautogui.hotkey('alt','f4')
-                break
+                #break   
+    
 
                 
