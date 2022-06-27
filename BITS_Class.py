eb296171e1f059e8c8622681ff134998532abfdd
Spyder96 <Amirul.1216@gmail.com>
2022-06-27 05:58:13 +0530

    fixed long sleep issue

--- BITS_Class.py
+++ BITS_Class.py
@@ -27,57 +27,47 @@ time_left=0
 is_class_started =False
 
 for lecture  in lst:
-    
-    # Checking for end timing of the selected class and skipping if required
+    while True:
+    #Checking for end timing of the selected class and skipping if required
     #Program run after the end of selected class
-    if ((datetime.now().hour*100) + datetime.now().minute) >= lecture[2]:
-        i+=1;
-        continue
-    # while True :
-    #     if is_class_started==False:
-    #         if ((datetime.now().hour*100) + datetime.now().minute) >= lecture[2]:
-    #             break;
-    
-    #Checking for end timing of the selected class and joining if time left
-    #Program run during the selected class
-    elif (datetime.now().hour*100 + datetime.now().minute >= lecture[1]) and (((datetime.now().hour*100) + datetime.now().minute) < lecture[2]):
+        if ((datetime.now().hour*100) + datetime.now().minute) >= lecture[2]:
+            i+=1
+            break
+        # while True :
+        #     if is_class_started==False:
+        #         if ((datetime.now().hour*100) + datetime.now().minute) >= lecture[2]:
+        #             break;
         
-         wb.open(lecture[0])
-         is_class_started=True
-         time.sleep(20)
-         for i in range (7):
-             pyautogui.press('tab')
-         time.sleep(1)
-         pyautogui.press('enter')
-         time.sleep(5)
-         time_left=lecture[2]-lecture[1]
-        # pyautogui.hotkey('ctrl','shift','m')
-        
-         time.sleep(time_left*10)       #class joined, sleeping for the class period
-         
-         #exiting
-         is_class_started=False
-         pyautogui.hotkey('ctrl','shift','h')
-         time.sleep(3)
-         pyautogui.hotkey('alt','f4')
-         time.sleep(3)
-         pyautogui.hotkey('alt','f4')
-         
-         
-    #Program run before the selected class timings     
-    elif   (datetime.now().hour*100 + datetime.now().minute < lecture[1]):
-        
-        time_left=lecture[1]-(datetime.now().hour*100 + datetime.now().minute)
-        time.sleep(time_left*10) 
-                # is_class_started=False
-                # pyautogui.hotkey('ctrl','shift','h')
-                # time.sleep(3)
-                # pyautogui.hotkey('alt','f4')
-                # time.sleep(3)
-                # pyautogui.hotkey('alt','f4')
-                #time.sleep(3)
-                #pyautogui.hotkey('alt','f4')
-                #break   
-    
+        #Checking for end timing of the selected class and joining if time left
+        #Program run during the selected class
+        elif (datetime.now().hour*100 + datetime.now().minute >= lecture[1]) and (((datetime.now().hour*100) + datetime.now().minute) < lecture[2]):
+            
+             wb.open(lecture[0])
+             is_class_started=True
+             time.sleep(30)
+             for i in range (7):
+                 pyautogui.press('tab')
+             time.sleep(1)
+             pyautogui.press('enter')
+             time.sleep(5)
+             
+        #Program run before the selected class timings     
+        elif   (datetime.now().hour*100 + datetime.now().minute < lecture[1]):
+            
+            time_left=lecture[1]-(datetime.now().hour*100 + datetime.now().minute)
+            time.sleep(time_left*10) 
+            
+        while is_class_started:
+            time_left=lecture[2]-(datetime.now().hour*100 + datetime.now().minute)
+            time.sleep(time_left*10)       #class joined, sleeping for the class period
+            
+            if time_left ==0:              #exiting
+                is_class_started=False
+                pyautogui.hotkey('ctrl','shift','h')
+                time.sleep(3)
+                pyautogui.hotkey('alt','f4')
+                time.sleep(3)
+                pyautogui.hotkey('alt','f4')
+                break;
 
                 
